29c89e46ecb95d42c81da1dce30dbd437c7748b5
Kaja K <Kaja.koren.97@gmail.com>
2020-03-19 12:08:11 +0100

    oppdat oppg1

--- oppgave1.m
+++ oppgave1.m
@@ -1,5 +1,16 @@
 [files, dur, F0s, F1s, F2s, F3s, F4s, F120, F220, F320, F150, F250, F350, F180, F280, F380] = textscan('vowdata_nohead.dat', '%s%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f%4.1f');
 
+vowels= char('ae', 'ah', 'aw', 'eh', 'er', 'ei', 'ih', 'iy', 'oa', 'oo', 'uh', 'uw'); 
+talker_group = char('m', 'w', 'b', 'g'); 
+filenames = char(filenames); %converts cell array to character matrix.
+[nfiles, nchar] = size(filenames); 
+
+for ifile=1:nfiles,
+    vowel_code(ifile) strmatch(filenames(ifile, 4:5), vowel); 
+    
+    
+
+
 
 
 
