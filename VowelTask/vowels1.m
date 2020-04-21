a43e2f18519125938b16800fae9758e99a4690c6
Solveig Reppen Lunde <solverl@stud.ntnu.no>
2020-04-21 11:29:29 +0200

    ferdig oppgave 1

--- VowelTask/vowels1.m
+++ VowelTask/vowels1.m
@@ -82,68 +82,23 @@ disp('Mean F0 for males:')
 % Sample mean for hver klasse
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[mean_trainF150, mean_test150] = find_mean(F150,Nclass,N,Ntrain);
-[mean_trainF250, mean_test250] = find_mean(F250,Nclass,N,Ntrain);
-[mean_trainF350, mean_test350] = find_mean(F350,Nclass,N,Ntrain);
+[mean_trainF1, mean_testF1] = find_mean(F1s,Nclass,N,Ntrain);
+[mean_trainF2, mean_testF2] = find_mean(F2s,Nclass,N,Ntrain);
+[mean_trainF3, mean_testF3] = find_mean(F3s,Nclass,N,Ntrain);
 
-%{
-%finner middelverdien til f1 for hver vokalklasse, lagrer disse i en felles vektor
-means_F1_train = zeros(Nclass,1);     %kan gjøre tilsvarende for F1, F2, F3
-means_F1_test = zeros(Nclass,1);
-for i=1:12
-    y = F1s(find(vowel_code==i));
-    means_F1_train(i,1) = mean(y(1:Ntrain));
-    means_F1_test(i,1) = mean(y(Ntrain+1:N));
-end
-
-%middelverdivektor for F2
-means_F2_train = zeros(Nclass,1);     
-means_F2_test = zeros(Nclass,1);
-for i=1:12
-    y = F2s(find(vowel_code==i));
-    means_F2_train(i,1) = mean(y(1:Ntrain));
-    means_F2_test(i,1) = mean(y(Ntrain+1:N));
-end
-
-%middelverdivektor for F3
-means_F3_train = zeros(Nclass,1);     
-means_F3_test = zeros(Nclass,1);
-for i=1:12
-    y = F3s(find(vowel_code==i));
-    means_F3_train(i,1) = mean(y(1:Ntrain));
-    means_F3_test(i,1) = mean(y(Ntrain+1:N));
-end
-%}
 
 %12x3 matrise som inneholder gjennomsnittsverdien til f1, f2 og f3 for hver
 %klasse
-means_train = [mean_trainF150 mean_trainF250 mean_trainF350];
-means_test = [mean_test150 mean_test250 mean_test350];
+means_train = [mean_trainF1 mean_trainF2 mean_trainF3];
+means_test = [mean_testF1 mean_testF2 mean_testF3];
 
 %lager (12*70)x3 matrise for til trening av klassifisereren
-f_50 = make_string(F150,F250,F350,N,Ntrain,Nclass);
+Fs = make_string(F1s,F2s,F3s,N,Ntrain,Nclass);
 
-%{
-Fs = [F1s F2s F3s];
-
-F_50 = [F150 F250 F350];
-F_50_train = zeros(Ntrain*Nclass,3);
-for i = 1:Nclass
-    x_string = F_50((i*N-N)+1:(i-1)*N+Ntrain,:);
-    F_50_train((i*Ntrain-Ntrain)+1:i*Ntrain,:) = x_string;
-end
-
-%lager (12*70)x3 matrise for trainset
-strings = zeros(70*Nclass,3);
-for i = 1:Nclass
-    x_string = Fs((i*N-N)+1:(i-1)*N+Ntrain,:);
-    strings((i*Ntrain-Ntrain)+1:i*Ntrain,:) = x_string;
-end
-%}
 % (12*3)x3 matrise bestående av de 12 covarians matrisene
 cov_matrices = zeros(Nclass*Nfeatures,Nfeatures);
 for i = 1:Nclass
-    cov_matrices((i-1)*3+1:(i*3),:) = find_cov(f_50, i, Ntrain);
+    cov_matrices((i-1)*3+1:(i*3),:) = find_cov(Fs, i, Ntrain);
 end
 
 
@@ -151,58 +106,45 @@ end
 % 1b: designe gaussian classifier
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-g_all = zeros(3*12,3);
-for i = 1:Nclass
-    g_all((i*3-3)+1:i*3,:) = discriminant2(cov_matrices((i*3-3)+1:i*3,:),means_train(i,:)',f_50(1,:),Prob_w);
+% (70*12)x12 matrise som inneholder alle discriminant functions
+g_all = zeros(Ntrain*Nclass,Nclass);
+for i = 1:Ntrain*Nclass
+    for c = 1:Nclass
+        x = Fs(i,:);
+        mu = means_train(c,:);
+        cov_mat = cov_matrices((c-1)*Nfeatures+1:c*Nfeatures,:);
+        g_all(i,c) = discriminant2(cov_mat, mu, x, Nfeatures, Prob_w);
+    end
 end
-%testing the functions
-%{
-g_840_1 = discriminant(Nfeatures, cov_matrices(1:3,:), means_train(1,:)',strings(840,:));
-g_840_12 = discriminant(Nfeatures, cov_matrices(34:36,:), means_train(12,:)',strings(840,:));
-g_1 = discriminant(Nfeatures,cov_matrices(1:3,1:3),means_train(1,:)',strings(1,:));
-g_1_1 = discriminant2(cov_matrices(1:3,1:3),means_train(1,:)',strings(1,:));
-g_1_12 = discriminant2(cov_matrices(34:36,1:3),means_train(12,:)',strings(1,:));
-g_1_8 = discriminant2(cov_matrices(22:24,1:3),means_train(8,:)',strings(1,:));
-%}
-%{
-gs = zeros (3*12,3);
-for i = 1:Nclass
-    gs((i*3-3)+1:i*3,:) = discriminant2(cov_matrices((i*3-3)+1:i*3,:),means_train(i,:)',strings(1,:))*Prob_w;
-end
-%}
 
+%trener klassifisereren
+true_val = fill_in_truevalues(Nclass,Ntrain);
+trainset = zeros(840,1);
+for x = 1:Nclass*Ntrain
+    for c = 1:Nclass
+        if g_all(x,c) == max(g_all(x,:))
+            trainset(x,1)= c;
+        end
+    end
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Lage confusion matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%kode fra iris
-%{
-g_all_test = zeros(1,Nclass);
-trainset_class = zeros(1,Nclass);
-testset_est = zeros(1,Nclass);
-
-for x = 1:C*Ntest
-    for c = 1:C
-        if g_all_test(x,c) == max(g_all_test(x,:))
-            testset_est(x)= c;
-        end
-    end
-end
-%}
-
-%kode fra iris
-%{
-conf_matrix= zeros(Nclass); % trenger en tabell som er 12x12, en med sann klasse, en med plassering. 
 %fill inn confusion matrix: 
+conf_matrix= zeros(Nclass); % trenger en tabell som er 12x12, en med sann klasse, en med plassering. 
 %length_test=length(testset_class); 
-for t=1:length_test
-    x=testset_class(t); 
-    y=testset_est(t); 
+for t=1:Nclass*Ntrain
+    x=true_val(t); 
+    y=trainset(t); 
     conf_matrix(x,y)= conf_matrix(x,y) +1;
 end
 disp(conf_matrix);
-%}
+
+%finner error rate
+error = compute_error(Nclass,Ntrain,conf_matrix);
+
 
 %{
 mean_ae = find_mean(1,Ntrain,F1s);
@@ -216,19 +158,23 @@ function x_string = find_x(Ntot, Ntrain, class_num,vec)
     x_string = vec((class_num*Ntot-Ntrain)+1:(class_num-1)*Ntot+Ntrain,:);
 end
 %}
+
 function cov_matrix = find_cov(string, class_num, N)
     x_string = string((class_num*N-N)+1:class_num*N,:);
     cov_matrix = cov(x_string);
 end
-
+%{
 function g_i = discriminant(dim,cov_matrix, mu,x)
     g_i = -(dim/2)*log(2*pi)-0.5*log(abs(cov_matrix))-0.5*(x-mu)'*cov_matrix^(-1)*(x-mu);
 end
+%}
 
-function g_i = discriminant2(cov_matrix, mu, x, prior)
-    g_i = normpdf(x,mu,cov_matrix)*prior;
+function g_i = discriminant2(cov_matrix, mu, x,Nfeatures, prior)
+    frac = (sqrt(2*pi)^(Nfeatures)*det(cov_matrix))^(-1);
+    expo = exp(-0.5*(x-mu)*cov_matrix^(-1)*(x-mu)');
+    g_i = frac*expo*prior;
 end
-
+%prøve gmdistribution?
 
 function [mean_train, mean_test] = find_mean(string,Nclass,Ntot,Ntrain)
     mean_train = zeros(Nclass,1);
@@ -248,4 +194,25 @@ function matrix = make_string(string1,string2,string3,Ntot,Ntrain,Nclass)
         x_string = matrix_tot((i*Ntot-Ntot)+1:(i-1)*Ntot+Ntrain,:);
         matrix((i*Ntrain-Ntrain)+1:i*Ntrain,:) = x_string;
     end
+end
+
+function truevalues = fill_in_truevalues(C,N)
+truevalues=zeros(C*N,1);
+    for c = 1:C
+        for i = 1:N
+            truevalues(i+(c-1)*N,1) = c;
+        end
+    end
+end
+
+function error_rate = compute_error(C,N,conf_mat)
+sum_error=0;
+for i = 1:C
+    for j = 1:C
+        if j ~= i           
+            sum_error = sum_error +conf_mat(j,i);
+        end
+    end
+end
+error_rate = sum_error/(C*N);
 end
